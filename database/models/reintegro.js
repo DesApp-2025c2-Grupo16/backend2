53b4cce3654efe16a3a0a64fdf5ebfa5d3ec3e05
Nicolasg07 <httpsn1k0@gmail.com>
2025-11-05 03:06:33 -0300

    Correxion del modelo de reintegro y datos agregados en la db

--- database/models/reintegro.js
+++ database/models/reintegro.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Reintegro extends Model {
     /**
@@ -11,8 +9,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Reintegro.belongsTo(models.Afiliado)
-      Reintegro.belongsTo(models.Prestador)//el que maneja la solicitud
+      Reintegro.belongsTo(models.Afiliado);
+      Reintegro.belongsTo(models.Prestador);//el que maneja la solicitud
       //relacion con prestador que atendio el turno
     }
   }
@@ -68,7 +66,15 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Reintegro',
-    timestamps: false
+    timestamps: false,
+    defaultScope: {
+      include: [
+        {
+          association: 'Afiliado',
+          attributes: ['id', 'nombre', 'apellido']
+        }
+      ]
+    }
   });
   return Reintegro;
 };
