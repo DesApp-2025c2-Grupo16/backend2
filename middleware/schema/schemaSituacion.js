e021baf3aa17b2766bceee25d1198289a0d655e7
Nicolasg07 <httpsn1k0@gmail.com>
2025-11-05 21:28:14 -0300

    Correccion de validaciones

--- middleware/schema/schemaSituacion.js
+++ middleware/schema/schemaSituacion.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const schemaSituacion = Joi.object({
     descripcion: Joi.string().min(3).max(500).required(),
     fechaInicio: Joi.date().required(),
-    fechaFin: Joi.date()
+    fechaFin: Joi.alternatives().try(Joi.date(), Joi.valid(null)).required()
 })
 
 module.exports = schemaSituacion
